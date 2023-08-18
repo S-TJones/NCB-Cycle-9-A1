ace90ca5fa4f55c3136b89ab16f054adf8d25b4b
S-TJones <jones.shemar.sj@gmail.com>
2023-08-18 15:11:09 -0500

    Updated Products page
    Images don't load.

--- myNCBApp/src/pages/Products.tsx
+++ myNCBApp/src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonBadge, IonLabel, IonCheckbox, IonNote } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonBadge, IonLabel, IonCheckbox, IonNote, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 
 const Products: React.FC = () => {
@@ -27,6 +27,47 @@ const Products: React.FC = () => {
                 <IonBadge color="success">5 Days</IonBadge>
             </IonItem>
         </IonList>
+
+        <IonCard>
+            <img src="pencil-pack.jpg" alt="Product 1" />
+            <IonCardHeader>
+                <IonCardSubtitle>Stationary</IonCardSubtitle>
+                <IonCardTitle>Pack of Pencils 1</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                Price: $X.XX
+            </IonCardContent>
+        </IonCard>
+        <IonCard>
+            <img src="small-speaker.jpg" alt="Product 2" />
+            <IonCardHeader>
+                <IonCardSubtitle>Tech</IonCardSubtitle>
+                <IonCardTitle>Small Speaker</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                Price: $X.XX
+            </IonCardContent>
+        </IonCard>
+        <IonCard>
+            <img src="thumb-drives.jpg" alt="Product 3" />
+            <IonCardHeader>
+                <IonCardSubtitle>Tech</IonCardSubtitle>
+                <IonCardTitle>Thumb Drive</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                Price: $X.XX
+            </IonCardContent>
+        </IonCard>
+        <IonCard>
+            <img src="water-bottle-mira.jpg" alt="Product 4" />
+            <IonCardHeader>
+                <IonCardSubtitle>Bottles</IonCardSubtitle>
+                <IonCardTitle>Water Bottle</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                Price: $X.XX
+            </IonCardContent>
+        </IonCard>
 
         <ExploreContainer />
       </IonContent>
